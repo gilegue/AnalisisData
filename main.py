02db2668f9e0b8f8d53a6b352217270d80106c1a
visynu <visynu.budhi@bpk.go.id>
2018-12-10 16:52:51 +0700

    Building Block Aplikasi

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Program untuk menganalisis data dari suatu sumber, diproses dan ditampilkan hasilnya
+"""
+
+from acquire import twitter
+from process import decision_tree
+from views import text
+
+data = twitter.get_data() #acquire
+result = decision_tree.process_data(data) #process
+display = text.as_text(result) #views
+
+print(display)
